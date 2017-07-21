86ac78d37c37f2a73da8c9c0f500d3ea3cdf6e25
gwens <gwenanspearing@hotmail.com>
2017-07-21 12:58:42 +0200

    Basic data import with D3

--- yesmate/js/index.js
+++ yesmate/js/index.js
@@ -2,3 +2,7 @@ import * as d3 from 'd3v4';
 
 const square = d3.selectAll("rect");
 square.style("fill", "orange"); 
+
+d3.json("/js/freq-data.json", function(data){
+    console.log("data is " + data.basicFreqSpec);
+});
