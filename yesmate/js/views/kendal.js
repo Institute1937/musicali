4d1ce569073281df5a0083dad88687f1269a8328
cerico <garethrobertlee@gmail.com>
2017-07-21 12:58:08 +0100

    a hyperapp view

---
+++ yesmate/js/views/kendal.js
@@ -0,0 +1,7 @@
+import { h } from 'hyperapp';
+
+export default (state, msg) => (
+  <div class="counter">
+    I come from Hyperapp - a minimal framework similar to react dont worry about me just yet
+  </div>
+);
