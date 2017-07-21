c3bc4983a5dfcbfe47acf891bfd63c0f0852ef89
cerico <garethrobertlee@gmail.com>
2017-07-21 12:58:43 +0100

    importing hydro into main index.js - alongside d3

--- yesmate/js/index.js
+++ yesmate/js/index.js
@@ -1,4 +1,10 @@
 import * as d3 from 'd3v4'; 
+import { app } from 'hyperapp';
+import view from './views/kendal';
 
 const square = d3.selectAll("rect");
 square.style("fill", "orange"); 
+
+
+////Hyperapp stuff
+app({ view });
