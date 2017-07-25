1b36a8c78469848e464a1a2336269f839dc37d66
gwens <gwenanspearing@hotmail.com>
2017-07-25 13:15:01 +0200

    Started afresh with D3. Moved svg out of html into index.js. Appending bars to the svg but all fixed height and position currently. Removed the black/orange boxes.

--- yesmate/js/index.js
+++ yesmate/js/index.js
@@ -1,9 +1,22 @@
 import * as d3 from 'd3v4'; 
 
-const square = d3.selectAll("rect");
-square.style("fill", "orange"); 
-
 d3.json("/js/freq-data.json", function(data){
-    console.log("data is " + data.basicFreqSpec[0].amount);
-    d3.select(".chart").append("rect").attr("width", 50).attr("height", (data.basicFreqSpec[0].amount * 10)).style("fill", "blue");
+    console.log("data is " + data.basicFreqSpec[0].amount);    
 });
+
+var freqs = [30, 50, 75, 22, 95]; //data.basicFreqSpec
+
+var svg = d3.select("body")
+            .append("svg")
+              .attr("width", 500)
+              .attr("height", 400);
+            
+svg.selectAll("rect")
+   .data(freqs)
+   .enter()
+   .append("rect")
+     .attr("x", "20px")
+     .attr("y", "20px")
+     .attr("width", "30px")
+     .attr("height", "320px")
+     .style("fill", "seagreen");
