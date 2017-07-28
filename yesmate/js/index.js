517fae62375cd5503b01c6ffc865364ade6c65fa
gwens <gwenanspearing@hotmail.com>
2017-07-28 12:18:45 +0200

    Moved bar chart logic to a separate function and got rid of the global data variable.

--- yesmate/js/index.js
+++ yesmate/js/index.js
@@ -1,12 +1,30 @@
 import * as d3 from 'd3v4'; 
 
-// Create global variable to hold data
-var ds; 
-
 // Set size of chart and bar padding
 var w = 500;
 var h = 400;
 var padding = 2;
+
+// Function for building a bar chart
+function buildBarChart(data){
+    
+    // Append the main svg for the chart to the page
+    var svg = d3.select("body")
+                .append("svg")
+                  .attr("width", w)
+                  .attr("height", h);
+
+    // Append the bars to the chart
+    svg.selectAll("rect")
+      .data(data)
+      .enter()
+      .append("rect")
+        .attr("x", function(d, i){ return (i * (w / data.length));})
+        .attr("y", function(d, i){ return (h - (d.amount * h) / 10);}) // Vertical scale fixed
+        .attr("width", w / data.length - padding)
+        .attr("height", function(d, i){ return (d.amount * h / 10);})
+        .style("fill", "seagreen");
+}
 
 // Import the frequency data from freq-data.json
 d3.json("/js/freq-data.json", function(error, data){
@@ -15,26 +33,6 @@ d3.json("/js/freq-data.json", function(error, data){
     } 
     else{
         var freqs = data.basicFreqSpec;
-        
-        // Append the main svg for the chart to the page
-        var svg = d3.select("body")
-                    .append("svg")
-                      .attr("width", w)
-                      .attr("height", h);
-
-        // Append the bars to the chart
-        svg.selectAll("rect")
-          .data(freqs)
-          .enter()
-          .append("rect")
-            .attr("x", function(d, i){ return (i * (w / freqs.length));})
-            .attr("y", function(d, i){ return (h - (d.amount * h) / 10);}) // Vertical scale fixed
-            .attr("width", w / freqs.length - padding)
-            .attr("height", function(d, i){ return (d.amount * h / 10);})
-            .style("fill", "seagreen");
+        buildBarChart(freqs);
     }
 });
-
-// Temporary simplified data array
-var freqs = [30, 50, 75, 22, 95]; //data.basicFreqSpec
-
