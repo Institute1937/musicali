c04573a13e872eab9fde68985e747729c36eb1d2
gwens <gwenanspearing@hotmail.com>
2017-08-24 14:57:38 +0200

    Modular version works but with repetition of constant declarations

--- yesmate/js/seesound/index.js
+++ yesmate/js/seesound/index.js
@@ -4,9 +4,14 @@ class Seesound {
 
     // Function for building a bar chart
     buildBarChart(data){
-        var w = 500;
-        var h = 400;
-        var padding = 2;
+        var self = this;
+        const w = 500;
+        const h = 400;
+        const padding = 2;
+
+        // Remove any existing svg
+        d3.select('svg').remove();
+
         // Append the main svg for the chart to the page
         var svg = d3.select("body")
             .append("svg")
@@ -18,11 +23,38 @@ class Seesound {
             .data(data)
             .enter()
             .append("rect")
-            .attr("x", function(d, i){ return (i * (w / data.length));})
-            .attr("y", function(d, i){ return (h - (d.amount * h) / 10);}) // Vertical scale fixed
-            .attr("width", w / data.length - padding)
-            .attr("height", function(d, i){ return (d.amount * h / 10);})
-            .style("fill", "orange");
+              .attr('class', function(d) { return (".band".concat(d.band)); })
+              .classed('.bar', true)
+              .attr("x", function(d, i){ return (i * (w / data.length));})
+              .attr("width", w / data.length - padding)
+              .attr("y", h) 
+              .attr("height", 10)
+              .style("fill", "orange");
+
+        // Animate the chart
+        self.animateChart(data);
+    }
+
+    animateChart(data){
+        const w = 500;
+        const h = 400;
+        const padding = 2;
+        var svg = d3.select('svg');
+
+        // Grab all the rectangles
+        var bars = svg.selectAll('rect').data(data);
+
+        // Change their attributes
+        step(0);
+
+        function step(i) {
+        bars
+            .transition()
+            .duration(1000)
+            .attr('y', function(d) { return (h - (d.amount[i] * h) / 10); })
+            .attr('height', function(d) { return (d.amount[i] * h / 10); })        
+            .on('end', function(d) { if (d.amount[i + 1]) { step(i + 1); } }); // Keep animating until the data runs out
+        }
     }
 
     createChart(){
@@ -32,7 +64,7 @@ class Seesound {
                 console.log(error);
             } 
             else{
-                var freqs = data.basicFreqSpec;
+                var freqs = data.tvaryingFreqSpec;
                 self.buildBarChart(freqs)        
             }
         });
