52af86ca07f5afdb4d937d73e57caa3ba2212d73
gwens <gwenanspearing@hotmail.com>
2017-08-29 11:34:40 +0200

    Removed duplicate variable definition by using a constructor instead. Fixed most linting errors.

--- yesmate/js/seesound/index.js
+++ yesmate/js/seesound/index.js
@@ -1,75 +1,75 @@
 import * as d3 from 'd3v4'; 
 
 class Seesound {
+  constructor(w, h, padding) {
+    this.w = w;
+    this.h = h;
+    this.padding = padding;
+  }
 
     // Function for building a bar chart
-    buildBarChart(data){
-        var self = this;
-        const w = 500;
-        const h = 400;
-        const padding = 2;
+  buildBarChart(data) {
+    const self = this;
 
-        // Remove any existing svg
-        d3.select('svg').remove();
+    // Remove any existing svg
+    d3.select('svg').remove();
 
-        // Append the main svg for the chart to the page
-        var svg = d3.select("body")
-            .append("svg")
-            .attr("width", w)
-            .attr("height", h);
+    // Append the main svg for the chart to the page
+    const svg = d3.select('body')
+        .append('svg')
+        .attr('width', this.w)
+        .attr('height', this.h);
 
         // Append the bars to the chart
-        svg.selectAll("rect")
-            .data(data)
-            .enter()
-            .append("rect")
-              .attr('class', function(d) { return (".band".concat(d.band)); })
-              .classed('.bar', true)
-              .attr("x", function(d, i){ return (i * (w / data.length));})
-              .attr("width", w / data.length - padding)
-              .attr("y", h - 10) 
-              .attr("height", 10)
-              .style("fill", "orange");
+    svg.selectAll('rect')
+        .data(data)
+        .enter()
+        .append('rect')
+            .attr('class', function(d) { return ('.band'.concat(d.band)); })
+            .classed('.bar', true)
+            .attr('x', function(d, i) { return (i * (self.w / data.length)); })
+            .attr('width', self.w / data.length - self.padding)
+            .attr('y', self.h - 10)
+            .attr('height', 10)
+            .style('fill', 'orange');
 
         // Animate the chart
-        self.animateChart(data);
+    self.animateChart(data);
+  }
+
+  animateChart(data) {
+    const self = this;
+    const svg = d3.select('svg');
+
+    // Grab all the rectangles
+    const bars = svg.selectAll('rect').data(data);
+
+    // Function to transition to the next data point and recur
+    function step(i) {
+      bars
+        .transition()
+        .duration(1000)
+        .attr('y', function(d) { return (self.h - (d.amount[i] * self.h) / 10); })
+        .attr('height', function(d) { return (d.amount[i] * self.h / 10); })
+        .on('end', function(d) { if (d.amount[i + 1]) { step(i + 1); } }); // Keep animating until the data runs out
     }
+    // Start the animation
+    step(0);
+  }
 
-    animateChart(data){
-        const w = 500;
-        const h = 400;
-        const padding = 2;
-        var svg = d3.select('svg');
-
-        // Grab all the rectangles
-        var bars = svg.selectAll('rect').data(data);
-
-        // Change their attributes
-        step(0);
-
-        function step(i) {
-        bars
-            .transition()
-            .duration(1000)
-            .attr('y', function(d) { return (h - (d.amount[i] * h) / 10); })
-            .attr('height', function(d) { return (d.amount[i] * h / 10); })        
-            .on('end', function(d) { if (d.amount[i + 1]) { step(i + 1); } }); // Keep animating until the data runs out
-        }
-    }
-
-    createChart(){
-        var self = this
-        d3.json("/js/freq-data.json", function(error, data){
-            if(error){
-                console.log(error);
-            } 
-            else{
-                var freqs = data.tvaryingFreqSpec;
-                self.buildBarChart(freqs)        
-            }
-        });
-    }  
+  createChart() {
+    const self = this;
+    d3.json('/js/freq-data.json', function(error, data) {
+      if (error) {
+        console.log(error);
+      }
+      else {
+        const freqs = data.tvaryingFreqSpec;
+        self.buildBarChart(freqs);
+      }
+    });
+  }
 }
 
-export let seesound = new Seesound();
+export const seesound = new Seesound(500, 400, 2);
 
